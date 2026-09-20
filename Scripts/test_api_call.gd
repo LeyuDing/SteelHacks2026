f4de76e302d59a7bab14e248b44125b1005936d9
hellothere98765 <64290251+hellothere98765@users.noreply.github.com>
2026-09-20 00:07:05 -0400

    works i think

--- Scripts/test_api_call.gd
+++ Scripts/test_api_call.gd
@@ -6,10 +6,21 @@ func _ready()->void:
 
 	print("RESPONSE:")
 	print(result["response"])
-
+	print("EMOTION:")
+	print(result["emotion"])
 	print("\nJSON:")
 	print(result["json"])
+	
+	result = await ApiCall.ask("Wow! You're so cool.", result["json"])
 
+	print("RESPONSE:")
+	print(result["response"])
+	print("EMOTION:")
+	print(result["emotion"])
+	print("\nJSON:")
+	print(result["json"])
+	
+	
 	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
