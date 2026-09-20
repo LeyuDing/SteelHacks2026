37c3e2da0f2b45b4a8e887743a08e301c662fc07
hellothere98765 <64290251+hellothere98765@users.noreply.github.com>
2026-09-20 00:00:32 -0400

    Safeguards working

--- Scripts/test_api_call.gd
+++ Scripts/test_api_call.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready()->void:
-	var result: Dictionary = await ApiCall.ask("Wow! You're so cool.")
+	var result: Dictionary = await ApiCall.ask("Wow! You're so cool.", [])
 
 	print("RESPONSE:")
 	print(result["response"])
